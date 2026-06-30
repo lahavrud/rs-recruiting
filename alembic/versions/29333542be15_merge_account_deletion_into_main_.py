299758498a4bf31c1f939349e088dfc7fc57ba90
Lahav Rudik <lahavrud@gmail.com>
2026-06-30 10:33:44 +0300

    chore(migrations): fix unused imports in generated merge migration

--- alembic/versions/29333542be15_merge_account_deletion_into_main_.py
+++ alembic/versions/29333542be15_merge_account_deletion_into_main_.py
@@ -5,15 +5,12 @@ Revises: 04c996bcec75, c5d6e7f8a9b0
 Create Date: 2026-06-30 10:28:25.625856
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '29333542be15'
-down_revision: Union[str, Sequence[str], None] = ('04c996bcec75', 'c5d6e7f8a9b0')
+revision: str = "29333542be15"
+down_revision: Union[str, Sequence[str], None] = ("04c996bcec75", "c5d6e7f8a9b0")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
